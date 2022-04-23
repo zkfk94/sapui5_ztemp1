3c35a3d750666040285c7398f972e4304c6bc706
zkfk94 <zkfk95123@gmail.com>
2022-04-23 10:57:09 +0900

    ztemp1

---
+++ README.md
@@ -0,0 +1,36 @@
+## Application Details
+|               |
+| ------------- |
+|**Generation Date and Time**<br>Sat Feb 19 2022 09:51:49 GMT+0900 (대한민국 표준시)|
+|**App Generator**<br>@sap/generator-fiori|
+|**App Generator Version**<br>1.4.5|
+|**Generation Platform**<br>Visual Studio Code|
+|**Floorplan Used**<br>simple|
+|**Service Type**<br>SAP System (ABAP On Premise)|
+|**Service URL**<br>http://210.16.199.140:8001/sap/opu/odata/sap/ZCL_M_05_SRV
+|**Module Name**<br>ztemp1|
+|**Application Title**<br>App Title|
+|**Namespace**<br>|
+|**UI5 Theme**<br>sap_fiori_3|
+|**UI5 Version**<br>Latest|
+|**Enable Code Assist Libraries**<br>False|
+|**Add Eslint configuration**<br>False|
+|**Enable Telemetry**<br>True|
+
+## ztemp1
+
+A Fiori application.
+
+### Starting the generated app
+
+-   This app has been generated using the SAP Fiori tools - App Generator, as part of the SAP Fiori tools suite.  In order to launch the generated app, simply run the following from the generated app root folder:
+
+```
+    npm start
+```
+
+#### Pre-requisites:
+
+1. Active NodeJS LTS (Long Term Support) version and associated supported NPM version.  (See https://nodejs.org)
+
+
---
+++ webapp/Component.js
@@ -0,0 +1,51 @@
+sap.ui.define([
+        "sap/ui/core/UIComponent",
+        "sap/ui/Device",
+        "ztemp1/model/models",
+        "sap/ui/model/json/JSONModel"
+    ],
+    function (UIComponent, Device, models, JSONModel) {
+        "use strict";
+
+        return UIComponent.extend("ztemp1.Component", {
+            metadata: {
+                manifest: "json"
+            },
+
+            /**
+             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+             * @public
+             * @override
+             */
+            init: function () {
+                // call the base component's init function
+                UIComponent.prototype.init.apply(this, arguments);
+
+                // enable routing
+                this.getRouter().initialize();
+
+                // set the device model
+                this.setModel(models.createDeviceModel(), "device");
+
+                var oModel = new JSONModel({
+                    create : {
+                        carrid : '',
+                        connid : '',				
+                    }
+                    ,filter : {
+                        carrid : '',
+                        connid : '',
+                        fldate : ''
+                    },
+                    it_data  : [],
+                    editMode : false
+                });
+
+                this.setModel(oModel, "main");
+
+                    }
+                });
+            }
+        });
+    }
+);
